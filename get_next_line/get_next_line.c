9c7ec04f09fd4aed14b7942df599befea2023f63
Cailing Fang <cafang@u91z08s02.42kl.edu.my>
2025-06-07 17:39:24 +0800

    add gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,16 +1,68 @@
 #include "get_next_line.h"
-/* clear static char *re_str to receive remianing string
+
+/* if we already  have something in storage, we use strjoin
+    if nothing in storage, we use strdup to creat new.
+
 */
+static  int read_update(int fd, char **storage, char *temp)
+{
+    char *buffer;
+    ssize_t len;
 
+    while (1)
+    {
+        len = read(fd, temp, BUTTER_SIZE)
+        if (len <= 0)
+            return (len);
+        temp[len] = '\0';
+        if (*storage)
+        {
+            buffer = *storage;
+            *storage = ft_strjoin(*storage, temp);
+            free(buffer);
+        }
+        else
+            *storage = ft_strdup(temp);
+        if (!(*storage))
+            return (-1);
+        if (ft_strchr(*storage, '\n'))
+            break;
+        return (len); 
+    }
+}
 
+static char get_line(char **storage)
+{
+    
+}
+
+/* clear static char *storage to store the remaining str behind \n
+    after read(). 
+
+*/
 char    *get_next_line(int fd)
 {
-    static char *re_str;
+    static char *storage;
+    char        *temp;
+    char        *new_line;
+    int         len;
 
     if (fd < 0 || BUFFER_SIZE <= 0)
-        return (0);
-    re_str = 
+        return (NULL);
+    temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
     if (!re_str)
         return (NULL);
-    
+    len = read_update(fd, &storage, temp);
+    free (temp);
+    temp = NULL;
+    if (!storage)
+        return (NULL);
+    if (count == 0 && ft_strlen(storage) == 0)
+    {
+        free(storage);
+        storage = NULL;
+        return (NULL);
+    }
+    new_line = get_line(&storage);
+    return (new_line);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,3 +10,62 @@ size_t  ft_strlen(const char *s)
     return (len);
 }
 
+char    *ft_strjoin(char const *s1, char const*s2)
+{
+    char    *str;
+    char    *ret;
+    int     i;
+    int     j;
+
+    i = 0;
+    j = 0;
+    if (!s1 || !s2)
+        return (0);
+    while (s1[i] != '\0')
+        i++;
+    while (s2[i] != '\0')
+        j++;
+    str = malloc((i + j + 1) * sizeof(char));
+    if (!str)
+        return (NULL);
+    ret = str;
+    while (*s1)
+        *str++ = *s1++;
+    while (*s2)
+        *str++ = *s2++;
+    *str = '\0';
+    return (ret); 
+}
+
+char    *ft_strdup(const char *s1)
+{
+    char    *ret;
+    size_t  i;
+    size_t  j;
+
+    j = 0;
+    i = ft_strlen(s1);
+    ret = malloc((i + 1) * sizeof(char));
+    if (!ret)
+        return (NULL);
+    while (j < i)
+    {
+        ret[j] = s1[j];
+        j++;
+    }
+    ret[j] = '\0';
+    return (ret);
+}
+
+char    *ft_strchr(char const *s, int c)
+{
+    while (*s)
+    {
+        if (*s == (char)c)
+            return ((char *)s);
+            s++;
+    }
+    if (c == '\0')
+        return ((char *)s);
+    return (NULL);
+}
